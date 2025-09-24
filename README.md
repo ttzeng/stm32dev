eb4c3dd3144c072eb77cfe4adcb984134a75e710
Tonny Tzeng <tonny.tzeng@gmail.com>
2025-09-25 00:24:34 +0800

    Initialize the project with blinky example

---
+++ README.md
@@ -0,0 +1,21 @@
+# My STM32 Projects
+
+This repository contains a collection of experimental projects and learning exercises using STM32 microcontrollers.
+It serves as a playground for exploring STM32 features, peripherals, and development techniques.
+
+## STM32 Black Pill
+
+The STM32 Black Pill is a small, complete, and breadboard-friendly board based on STMicroelectronics's STM32F103C8T6 microcontroller, which integrates a high-performance, ARM® Cortex®-M3 32-bit RISC core operating at a 72 MHz frequency, high-speed embedded memories (Flash memory 64Kbytes and SRAM up to 20 Kbytes), and an extensive range of enhanced I/Os and peripherals connected to two APB buses. The board offers two 12-bit ADCs, three general purpose 16-bit timers plus one PWM timer, as well as standard and advanced communication interfaces such as I2Cs and SPIs, USARTs, USB and CAN.
+
+<img src="https://uglyduck.vajn.icu/STM32_Pills/BlackPill_STM32F103_Pinout.jpg" width="500px"/>
+
+![][4]
+
+## Resources
+
+* [STM32F103C8T6 Black Pill][1]
+
+[1]: https://stm32-base.org/boards/STM32F103C8T6-Black-Pill.html "STM32-base: the Black Pill board"
+[2]: https://uglyduck.vajn.icu/ep/archive/2020/11/STM32__Pill__boards.html "STM32 Pill boards in Emil's project"
+[3]: https://uglyduck.vajn.icu/STM32_Pills/BlackPill_STM32F103_Pinout.jpg "Black Pill Pinout"
+[4]: https://innovatorsguru.com/wp-content/uploads/2019/08/stm32-black-pill-schematic-800x506.jpg "Black Pill Schematics"
---
+++ blinky/Core/Src/main.c
@@ -0,0 +1,142 @@
+#include "main.h"
+#include "cmsis_os.h"
+
+#define LED_PIN  GPIO_PIN_12
+#define LED_PORT GPIOB
+
+/* Definitions for blinkyTask */
+osThreadId_t blinkyTaskHandle;
+const osThreadAttr_t blinkyTask_attributes = {
+  .name = "blinkyTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+void StartBlinkyTask(void *argument);
+
+int main(void)
+{
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* Create the thread(s) */
+  blinkyTaskHandle = osThreadNew(StartBlinkyTask, NULL, &blinkyTask_attributes);
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  while (1);
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+}
+
+void StartBlinkyTask(void *argument)
+{
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1000);
+	HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+}
+#endif /* USE_FULL_ASSERT */
